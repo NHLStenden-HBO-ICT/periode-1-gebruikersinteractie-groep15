9124be669e87274612e565b399c91003ca4f2c6d
Durk <durk.posthumus1@student.nhlstenden.com>
2023-10-13 13:49:16 +0200

    add project to github

---
+++ Prototype_Game Interaction/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prototype_Game_Interaction
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButtonClick(object sender, RoutedEventArgs e)
+        {
+            StartSpel startSpel = new StartSpel();
+            startSpel.Visibility = Visibility.Visible;
+            this.Visibility = Visibility.Hidden;
+        }
+        private void ScoreBoardClick(object sender, RoutedEventArgs e)
+        {
+            ScoreBoard scoreBoard = new ScoreBoard();
+            scoreBoard.Visibility = Visibility.Visible;
+            this.Visibility = Visibility.Hidden;
+        }
+
+        private void InstellingenClick(object sender, RoutedEventArgs e)
+        {
+            Instellingen instellingen = new Instellingen();
+            instellingen.Visibility = Visibility.Visible;
+            this.Visibility = Visibility.Hidden;
+        }
+        private void HelpClick(object sender, RoutedEventArgs e) 
+        {
+            Help help = new Help();
+            help.Visibility = Visibility.Visible;
+            this.Visibility = Visibility.Hidden;
+
+        }
+        private void QuitButtonClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        
+    }
+}
---
+++ Prototype_Game Interaction/StartSpel.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Prototype_Game_Interaction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Prototype_Game_Interaction
+{
+    /// <summary>
+    /// Interaction logic for StartSpel.xaml
+    /// </summary>
+    public partial class StartSpel : Window
+    {
+        public StartSpel()
+        {
+            InitializeComponent();
+        }
+        private void SpelenClick(object sender, RoutedEventArgs e)
+        {
+            GameWindow gameWindow = new GameWindow();
+            gameWindow.Visibility = Visibility.Visible;
+            this.Visibility = Visibility.Hidden;
+        }
+        private void MainWindowClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Visibility = Visibility.Visible;
+            this.Visibility = Visibility.Hidden;
+        }
+        private void InstellingenClick(object sender, RoutedEventArgs e)
+        {
+            Instellingen instellingen = new Instellingen();
+            instellingen.Visibility = Visibility.Visible;
+            this.Visibility = Visibility.Hidden;
+        }
+    }
+}
