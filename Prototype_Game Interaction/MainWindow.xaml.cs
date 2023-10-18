6d3225a7ef3de18a357a558db0cfac14bdecec49
Danielvbbh <danielvbeijeren@gmail.com>
2023-10-18 18:06:49 +0200

    Wijzigingen: Alle schermen starten in het midden. Instellingen scherm aangepast met design en functionaliteit.

--- Prototype_Game Interaction/MainWindow.xaml.cs
+++ Prototype_Game Interaction/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Prototype_Game_Interaction;
 
 namespace Prototype_Game_Interaction
 {
@@ -20,19 +21,25 @@ namespace Prototype_Game_Interaction
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            SharedData.CurrentScreen = "MainWindow";
         }
 
         private void StartButtonClick(object sender, RoutedEventArgs e)
         {
+            SharedData.CurrentScreen = "StartSpel";
+
             StartSpel startSpel = new StartSpel();
             startSpel.Visibility = Visibility.Visible;
             this.Visibility = Visibility.Hidden;
         }
         private void ScoreBoardClick(object sender, RoutedEventArgs e)
         {
+            SharedData.CurrentScreen = "ScoreBoard";
+
             ScoreBoard scoreBoard = new ScoreBoard();
             scoreBoard.Visibility = Visibility.Visible;
             this.Visibility = Visibility.Hidden;
@@ -40,12 +47,18 @@ namespace Prototype_Game_Interaction
 
         private void InstellingenClick(object sender, RoutedEventArgs e)
         {
+            SharedData.CurrentScreen = "Instellingen";
+
             Instellingen instellingen = new Instellingen();
             instellingen.Visibility = Visibility.Visible;
             this.Visibility = Visibility.Hidden;
+            
+
         }
         private void HelpClick(object sender, RoutedEventArgs e) 
         {
+            SharedData.CurrentScreen = "Help";
+
             Help help = new Help();
             help.Visibility = Visibility.Visible;
             this.Visibility = Visibility.Hidden;
---
+++ Prototype_Game Interaction/SharedData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_Game_Interaction
+{
+    public static class SharedData
+    {
+        public static string CurrentScreen { get; set; }
+    }
+}
