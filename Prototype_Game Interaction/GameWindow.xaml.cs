e13777e38c3d923e4a7809fbb22c55aa030b0972
GruttePot <d.pot431@gmail.com>
2023-10-26 15:10:41 +0200

    Afronden Hoofdmenu & StartSpel, Huisstyle toepassen, Begin aan GameWindow

--- Prototype_Game Interaction/GameWindow.xaml.cs
+++ Prototype_Game Interaction/GameWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using Prototype_Game_Interaction;
 
 
@@ -22,11 +24,79 @@ namespace Prototype_Game_Interaction
     /// </summary>
     public partial class GameWindow : Window
     {
+        private bool lift = true;
+        private DispatcherTimer GameTimer = new DispatcherTimer();
+        private ImageBrush player1Brush = new ImageBrush();
+        private ImageBrush player2Brush = new ImageBrush();
+
         public GameWindow()
         {
             InitializeComponent();
             this.KeyDown += PauzeMenu_keyDown; // voor de knop om naar het pauze menu te gaan
+            this.KeyDown += Button1_keyDown;
+
+
+            GameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            GameTimer.Tick += GameEngine;
+            GameTimer.Start();
         }
+
+
+
+
+        private void GameEngine(object? sender, EventArgs e)
+        {
+            // Controls
+            //Random random = new Random();
+            //int controls = Convert.ToInt32(lift);
+
+            //if (lift)
+               
+
+
+
+
+        }
+
+        //private void Player1Controls(object sender, KeyEventArgs e)
+        //{
+
+        //    // Controls Player 1
+        //    if (e.Key == Key.W)
+        //        lift = true;
+
+        //    if (e.Key == Key.S)
+        //        lift = true;
+
+        //    if (e.Key == Key.D)
+        //        lift = true;
+
+        //    if (e.Key == Key.A)
+        //        lift = true;
+
+        //}
+
+
+        //private void Player2Controls(object sender, KeyEventArgs e)
+        //{
+
+        //    // Controls Player 2
+
+        //    if (e.Key == Key.Up)
+        //        lift = true;
+
+        //    if (e.Key == Key.Down)
+        //        lift = true;
+
+        //    if (e.Key == Key.Right)
+        //        lift = true;
+
+        //    if (e.Key == Key.Left)
+        //        lift = true;
+
+        //}
+
+
 
         private void PauzeMenu_keyDown(object sender, KeyEventArgs e)
         {
@@ -42,5 +112,13 @@ namespace Prototype_Game_Interaction
             }
         }
 
+        private void Button1_keyDown(object sender, KeyEventArgs e)
+        {
+            // dit is als test om te kijken of de knop werkt
+            if (e.Key == Key.Enter)
+            {
+                //knop.Text = "Nee mag niet!!";
+            }
+        }
     }
 }
