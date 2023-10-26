9e058385a4a927bcd45258c1a7e2731a8be2cc31
Brent Tinga <brent.tinga.2004@gmail.com>
2023-10-26 15:08:56 +0200

    Animatie spull aan het maken en daarna de controls

--- Prototype_Game Interaction/GameWindow.xaml.cs
+++ Prototype_Game Interaction/GameWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 using Prototype_Game_Interaction;
+using static System.Net.Mime.MediaTypeNames;
 
 
 namespace Prototype_Game_Interaction
@@ -23,13 +24,25 @@ namespace Prototype_Game_Interaction
     /// </summary>
     public partial class GameWindow : Window
     {
+        private int frameIndex = 0;
+        private int frameWidth = 32;
+        private int frameHeight = 32;
+        private int totalFrames = 22;
+        private DispatcherTimer animationTimer;
 
         public GameWindow()
         {
             InitializeComponent();
             this.KeyDown += PauzeMenu_keyDown; // voor de knop om naar het pauze menu te gaan
             this.KeyDown += Button1_keyDown;
+            
+            // Start the animation timer
+            animationTimer = new DispatcherTimer();
+            animationTimer.Interval = TimeSpan.FromMilliseconds(18);
+            animationTimer.Tick += AnimationTimer_Tick;
+            animationTimer.Start();
         }
+    
 
         private void PauzeMenu_keyDown(object sender, KeyEventArgs e)
         {
@@ -53,5 +66,22 @@ namespace Prototype_Game_Interaction
                 knop.Text = "Nee mag niet!!";
             }
         }
+
+        private void AnimationTimer_Tick(object sender, EventArgs e)
+        {
+            // Update the frame index
+            frameIndex = (frameIndex + 1) % totalFrames;
+
+            // Calculate the position of the current frame in the sprite sheet
+            int x = frameIndex * frameWidth;
+            int y = 0;
+
+            // Create a new CroppedBitmap that displays the current frame
+            CroppedBitmap frame = new CroppedBitmap(new BitmapImage(new Uri("Assets/player1 curl-Sheet.png", UriKind.Relative)), new Int32Rect(x, y, frameWidth, frameHeight));
+
+            // Update the Source property of the MyImage control to display the current frame
+            Player1curl.Source = frame;
+        }
+
     }
 }
