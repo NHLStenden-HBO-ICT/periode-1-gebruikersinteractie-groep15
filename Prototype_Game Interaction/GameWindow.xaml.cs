54a14e11cd78597f0d1eb3bf5c240a6792e2614b
GruttePot <d.pot431@gmail.com>
2023-10-28 18:05:07 +0200

    Kleine aanpassing aan MainWindow & StartSpel, Verwijderen test code GameWindow

--- Prototype_Game Interaction/GameWindow.xaml.cs
+++ Prototype_Game Interaction/GameWindow.xaml.cs
@@ -24,90 +24,13 @@ namespace Prototype_Game_Interaction
     /// </summary>
     public partial class GameWindow : Window
     {
-        public bool lift1, lift2 = true;
-        private DispatcherTimer GameTimer = new DispatcherTimer();
-        private ImageBrush player1Brush = new ImageBrush();
-        private ImageBrush player2Brush = new ImageBrush();
-
-        private Key[] keysPlayer1 = { Key.W, Key.S, Key.D, Key.A };
-        private Key[] keysPlayer2 = { Key.Up, Key.Down, Key.Right, Key.Left };
-        public Key lift1Key, lift2Key;
-
 
         public GameWindow()
         {
             InitializeComponent();
             this.KeyDown += PauzeMenu_keyDown; // voor de knop om naar het pauze menu te gaan
             this.KeyDown += Button1_keyDown;
-
-
-          
-
-            this.KeyDown += new KeyEventHandler(Player1Controls);
-            this.KeyUp += new KeyEventHandler(Player1Controls);
-
-            this.KeyDown += new KeyEventHandler(Player2Controls);
-            this.KeyUp += new KeyEventHandler(Player2Controls);
-
-            GameTimer.Interval = TimeSpan.FromMilliseconds(20);
-            GameTimer.Tick += GameEngine;
-            GameTimer.Start();
         }
-
-
-
-
-        private void GameEngine(object? sender, EventArgs e)
-        {
-            // Controls
-            Random random = new Random();
-            lift1Key = keysPlayer1[random.Next(keysPlayer1.Length)];
-            lift2Key = keysPlayer2[random.Next(keysPlayer2.Length)];
-        }
-
-       
-
-
-        private void Player1Controls(object sender, KeyEventArgs e)
-        {
-
-            // Controls Player 1
-            if (e.Key == Key.W)
-                lift1 = true;
-
-            if (e.Key == Key.S)
-                lift1 = true;
-
-            if (e.Key == Key.D)
-                lift1 = true;
-
-            if (e.Key == Key.A)
-                lift1 = true;
-
-        }
-
-
-        private void Player2Controls(object sender, KeyEventArgs e)
-        {
-
-            // Controls Player 2
-
-            if (e.Key == Key.Up)
-                lift2 = true;
-
-            if (e.Key == Key.Down)
-                lift2 = true;
-
-            if (e.Key == Key.Right)
-                lift2 = true;
-
-            if (e.Key == Key.Left)
-                lift2 = true;
-
-        }
-
-
-
         private void PauzeMenu_keyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Escape)
