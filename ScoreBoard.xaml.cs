aa5f20594f5ae22e562805d012d5ae0eed04c2c8
sybrand420 <149199495+sybrand420@users.noreply.github.com>
2023-10-29 20:28:56 +0100

    Add files via upload

---
+++ ScoreBoard.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Prototype_Game_Interaction;
+
+namespace Prototype_Game_Interaction
+{
+    /// <summary>
+    /// Interaction logic for ScoreBoard.xaml
+    /// </summary>
+    public partial class ScoreBoard : Window
+    {
+        public ScoreBoard()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ZekerheidClick(object sender, RoutedEventArgs e)
+        {
+            // voor de knop om naar het begin menu te gaan maar je moet eerst nog dat bevestingen in een ander menu
+            SharedData.CurrentScreen = "ConformatieMenu";
+
+            ZekerheidMenu zekerheidMenu = new ZekerheidMenu();
+            zekerheidMenu.Visibility = Visibility.Visible;
+            this.Visibility = Visibility.Hidden;
+        }
+
+        }
+    }
+
+
