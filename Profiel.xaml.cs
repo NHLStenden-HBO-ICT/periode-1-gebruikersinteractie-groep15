7fa323c9204da83ff270935f0ac864acb8c7451e
sybrand420 <149199495+sybrand420@users.noreply.github.com>
2023-10-31 12:39:08 +0100

    profiel versie 1

---
+++ Profiel.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Prototype_Game_Interaction
+{
+    /// <summary>
+    /// Interaction logic for Profiel.xaml
+    /// </summary>
+    public partial class Profiel : Window
+    {
+        public Profiel()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void ZekerheidClick(object sender, RoutedEventArgs e)
+        {
+            // voor de knop om naar het begin menu te gaan maar je moet eerst nog dat bevestingen in een ander menu
+            SharedData.CurrentScreen = "ConformatieMenu";
+
+            ZekerheidMenu zekerheidMenu = new ZekerheidMenu();
+            zekerheidMenu.Visibility = Visibility.Visible;
+            this.Visibility = Visibility.Hidden;
+        }
+
+        private void Gebruikersnaam_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void mail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
