15d66518647254296e599d03374eb69ecbb6bf4d
GruttePot <d.pot431@gmail.com>
2023-11-03 18:19:08 +0100

    Revert "Add files via upload"
    This reverts commit 5abc3a113cc0174b603829dd4fbac427ee41904b.

--- Prototype_Game Interaction/ScoreBoard.xaml.cs
+++ Prototype_Game Interaction/ScoreBoard.xaml.cs
@@ -1,22 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.IO;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using Microsoft.Data.Sqlite;
 using Prototype_Game_Interaction;
-using Microsoft.Data.SqlClient;
-using System.Data.SQLite;
 
 namespace Prototype_Game_Interaction
 {
@@ -25,80 +20,29 @@ namespace Prototype_Game_Interaction
     /// </summary>
     public partial class ScoreBoard : Window
     {
-
         public ScoreBoard()
         {
             InitializeComponent();
             this.KeyDown += MainWindow_keyDown; // voor de knop om terug te gaan naar het hoofdmenu
-            FillDataGrid();
         }
-
-
-
-        private void FillDataGrid()
-        {
-            string relativePath = "GameScores.db";
-            string currentFolder = AppDomain.CurrentDomain.BaseDirectory;
-            string databasePath = System.IO.Path.Combine(currentFolder, relativePath);
-            string connectionString = $@"Data Source={databasePath};Version=3;";
-
-            using (var conn = new SQLiteConnection(connectionString))
-            {
-                try
-                {
-                    conn.Open();
-                    string cmdString = "SELECT Scores, Gebruikersnaam FROM Scores";
-                    using (var cmd = new SQLiteCommand(cmdString, conn))
-                    {
-                        using (var sda = new SQLiteDataAdapter(cmd))
-                        {
-                            DataTable dt = new DataTable("Scores");
-                            sda.Fill(dt);
-                            Scores.ItemsSource = dt.DefaultView;
-                        }
-                    }
-                }
-                catch (System.Data.SQLite.SQLiteException ex)
-                {
-                    MessageBox.Show($"An error occurred: {ex.Message}");
-                }
-                string cmdStringHighestScore = "SELECT MAX(Scores) AS HighestScore, Gebruikersnaam FROM Scores GROUP BY Gebruikersnaam ORDER BY HighestScore DESC LIMIT 1";
-                using (var cmdHighestScore = new SQLiteCommand(cmdStringHighestScore, conn))
-                {
-                    using (SQLiteDataReader reader = cmdHighestScore.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            HighestScore.Text = $"{reader["Gebruikersnaam"]}: {reader["HighestScore"]}";
-                        }
-                    }
-                }
-            }
-        }
-
-
-
         private void MainWindowClick(object sender, RoutedEventArgs e)
         {
             SharedData.CurrentScreen = "MainWindow";
+
             MainWindow mainWindow = new MainWindow();
-            mainWindow.Show();
-            this.Close();
+            mainWindow.Visibility = Visibility.Visible;
+            this.Visibility = Visibility.Hidden;
         }
 
         private void MainWindow_keyDown(object sender, KeyEventArgs e)
         {
+            // when the Esc key is pressed the screen will go back to the main menu
             if (e.Key == Key.Escape)
             {
                 MainWindow mainWindow = new MainWindow();
-                mainWindow.Show();
-                this.Close();
+                mainWindow.Visibility = Visibility.Visible;
+                this.Visibility = Visibility.Hidden;
             }
-        }
-        
-        private void Scores_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
         }
     }
 }
