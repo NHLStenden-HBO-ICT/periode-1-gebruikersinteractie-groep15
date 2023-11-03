384cc6b2508163071d4635249dfad8e598a54a90
sybrand420 <149199495+sybrand420@users.noreply.github.com>
2023-11-03 11:22:28 +0100

    Add files via upload

---
+++ Prototype_Game Interaction/change.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_Game_Interaction
+{
+    public static class change
+    {
+
+        public static string Text { get; set; }
+        public static string Text2 { get; set;}
+        public static string Text3 { get; set;}
+        public static string Text4 { get; set;}
+
+
+    }
+}
